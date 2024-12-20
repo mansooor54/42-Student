fa75bb8db8dc69385bb3050c0cacc7737016d842
mansooor54 <uaeriver@hotmail.com>
2024-12-21 01:52:48 +0400

    create main file for some file like ft_bzero, ft_isalnum, ft_isdigit and ft_isalpha

--- libft.a/ft_isalpha.c
+++ libft.a/ft_isalpha.c
@@ -18,3 +18,18 @@ int	ft_isalpha(int c)
 		return (1);
 	return (0);
 }
+
+int main()
+{
+    char c1 = 'A';  // Uppercase letter
+    char c2 = 'g';  // Lowercase letter
+    char c3 = '5';  // Digit
+    char c4 = '@';  // Special character
+
+    printf("Is '%c' an alphabet? %d\n", c1, ft_isalpha(c1)); // Output: 1
+    printf("Is '%c' an alphabet? %d\n", c2, ft_isalpha(c2)); // Output: 1
+    printf("Is '%c' an alphabet? %d\n", c3, ft_isalpha(c3)); // Output: 0
+    printf("Is '%c' an alphabet? %d\n", c4, ft_isalpha(c4)); // Output: 0
+
+    return 0;
+}
