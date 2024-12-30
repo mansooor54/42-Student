ef584eefde531d56fddb97b6bf0885e10325dc45
mansooor54 <uaeriver@hotmail.com>
2024-12-31 00:21:23 +0400

    solve List Function for Bounce

---
+++ libft.a/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 00:13:39 by marvin            #+#    #+#             */
+/*   Updated: 2024/12/31 00:13:39 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*node;
+
+	node = lst;
+	count = 0;
+	while (node != NULL)
+	{
+		count++;
+		node = node->next;
+	}
+	return (count);
+}
