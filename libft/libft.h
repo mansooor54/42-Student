b241ecdbda3b4efa01fe818260bc425a6c6d64d1
mansooor54 <uaeriver@hotmail.com>
2025-01-11 11:50:35 +0400

    insert <fcntl.h> liberary to write inside file

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: malmarzo <malmarzo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 00:32:35 by malmarzo          #+#    #+#             */
-/*   Updated: 2024/12/30 23:40:32 by malmarzo         ###   ########.fr       */
+/*   Updated: 2025/01/11 11:40:32 by malmarzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <ctype.h>
 # include <string.h>
+# include <fcntl.h>
 # include <unistd.h>
 
 typedef struct s_list
@@ -63,15 +64,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-//Linked list functions
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
 #endif
