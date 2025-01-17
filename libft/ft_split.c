9eaaa8d98d71cfc8fdf7ac64aa763ed8e4df7654
mansooor54 <uaeriver@hotmail.com>
2025-01-18 01:06:13 +0400

    compare groub 2 with other libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -14,14 +14,8 @@
 
 static void	free_memory(char **str, int index)
 {
-	int	i;
-
-	i = 0;
-	while (i <= index)
-	{
-		free(str[i]);
-		i++;
-	}
+	while (index >= 0)
+		free(str[index--]);
 	free(str);
 }
 
@@ -32,24 +26,24 @@ static int	count_word(char const *str, char c)
 
 	i = 0;
 	word_count = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (str[i] != '\0' && str[i] == c)
+		while (str[i] && str[i] == c)
 			i++;
-		if (str[i] != '\0')
+		if (str[i])
 			word_count++;
-		while (str[i] != '\0' && str[i] != c)
+		while (str[i] && str[i] != c)
 			i++;
 	}
 	return (word_count);
 }
 
-static int	word_strlen(char const *str, char c)
+static int	word_strlen(const char *str, char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != c)
+	while (str[i] && str[i] != c)
 		i++;
 	return (i);
 }
@@ -81,24 +75,21 @@ char	**ft_split(const char *s, char c)
 
 	i = 0;
 	new_array = (char **)ft_calloc((count_word(s, c) + 1), sizeof(char *));
-	if (new_array == NULL || s == NULL)
+	if (!new_array || !s)
 		return (NULL);
-	while (*s != '\0')
+	while (*s)
 	{
-		while (*s != '\0' && *s == c)
+		while (*s && *s == c)
 			s++;
-		if (*s != '\0')
+		if (*s)
 		{
 			new_array[i] = new_word(s, c);
 			if (new_array[i] == NULL)
-			{
-				free_memory(new_array, i);
-				return (NULL);
-			}
+				return (free_memory(new_array, i), NULL);
 			i++;
+			while (*s && *s != c)
+				s++;
 		}
-		while (*s != '\0' && *s != c)
-			s++;
 	}
 	return (new_array);
 }
